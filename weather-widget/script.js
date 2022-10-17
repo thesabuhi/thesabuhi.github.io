78477248237c64071e9ca6c0970423d56bd4b842
asdadasd <asasdasd@apsdjpasd.com>
2022-10-17 17:38:24 +0400

    Added some API

--- weather-widget/script.js
+++ weather-widget/script.js
@@ -1,16 +1,69 @@
+// Clock
+"use strict";
 function getClockTime() {
-    const d = new Date();
-    d.getHours();
-    d.getMinutes();
-    let clock;
-    if (d.getMinutes()<10) {
-        clock = d.getHours() + ':' + '0' + d.getMinutes();
-    } else {
-        clock = d.getHours() + ':' + d.getMinutes();
-    }
-    
-    document.getElementById("detail__time").innerHTML = clock;
+  const d = new Date();
+  d.getHours();
+  d.getMinutes();
+  let clock;
+  if (d.getMinutes() < 10) {
+    clock = d.getHours() + ":" + "0" + d.getMinutes();
+  } else {
+    clock = d.getHours() + ":" + d.getMinutes();
+  }
+
+  document.getElementById("detail__time").innerHTML = clock;
 }
 
-    getClockTime();
-    setInterval(getClockTime,500);
+getClockTime();
+setInterval(getClockTime, 500);
+
+getApiData("Moscow");
+
+// Input City (serach)
+document
+  .querySelector(".search-input")
+  .addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      const city = this.value;
+
+      const apiData = getApiData(city);
+    }
+  });
+
+// Fetch API
+function getApiData(city) {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "b8bcc3dfa0msh13afb966c0dbccap191d17jsneefcbcbcf97c",
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+  };
+
+  fetch(
+    `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=10`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      document.querySelector(".detail__city p").textContent =
+        response.location.name;
+      document.querySelector(".detail__country p").textContent =
+        response.location.country;
+      document.querySelector(
+        ".detail__weather-value p"
+      ).textContent = `${response.current.temp_c}°C`;
+      document.querySelector(".detail__weather-text p").textContent =
+        response.current.condition.text;
+      let weatherIcon = response.current.condition.icon;
+      document
+        .querySelector(".detail__weather-icon img")
+        .setAttribute("src", weatherIcon.replace("64x64", "128x128"));
+
+      console.log(response);
+    })
+
+    .catch((err) => {
+      console.error(err);
+    });
+}
